3a18916be007943c9abab215ff558b40fc91f59c
noah <noah@iamnoah.com>
2018-10-11 18:40:07 -0700

    micro simple d20s

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 console.info('d20-server');
 
-module.exports = () => 'roll a d20';
+module.exports = () => rolld20();
+
+module.exports = async (req, res) => {
+  const { path } = req;
+
+  console.log({ path });
+
+  switch(path) {
+    case 'party':
+      return `100k d20s...\n${JSON.stringify(d20party(), null, 2)}`;
+    default:
+      return rolld20();
+  }
+}
+
+
+function rolld20() {
+  return Math.floor(Math.random() * 20) + 1;
+}
+
+function d20party() {
+  const results = {};
+
+  for (let i = 0; i < 100000; i++) {
+    const result = rolld20();
+
+    // ensure result is initialized
+    if (typeof results[result] === 'undefined') results[result] = 0;
+
+    // increment this result
+    results[result]++;
+  }
+
+  return results;
+}
