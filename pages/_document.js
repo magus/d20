a5a37a9f1fc6d28932159b53d30251e6b041e619
Noah Jorgensen <noah@iamnoah.com>
2018-11-11 21:15:53 -0800

    use global styles components

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,26 @@
 // _document is only rendered on the server side and not on the client side
 // Event handlers like onClick can't be added to this file
-import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+import StyleReset from '~/app/components/StyleReset';
+import GlobalStyles from '~/app/components/GlobalStyles';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
 
     const originalRenderPage = ctx.renderPage;
-    ctx.renderPage = () => originalRenderPage({
-      enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-    });
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+      });
 
     const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps, styles: [...initialProps.styles, ...sheet.getStyleElement()] };
+    return {
+      ...initialProps,
+      styles: [...initialProps.styles, ...sheet.getStyleElement()]
+    };
   }
 
   render() {
@@ -22,65 +29,18 @@ export default class MyDocument extends Document {
         <Head>
           <meta httpEquiv="Referrer-Policy" content="no-referrer" />
           <meta name="referrer" content="no-referrer" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=3.0, minimum-scale=1" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=3.0, minimum-scale=1"
+          />
 
-          <style jsx global>{`
-            html, body, div, span, applet, object, iframe,
-            h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-            a, abbr, acronym, address, big, cite, code,
-            del, dfn, em, img, ins, kbd, q, s, samp,
-            small, strike, strong, sub, sup, tt, var,
-            b, u, i, center,
-            dl, dt, dd, ol, ul, li,
-            fieldset, form, label, legend,
-            table, caption, tbody, tfoot, thead, tr, th, td,
-            article, aside, canvas, details, embed,
-            figure, figcaption, footer, header, hgroup,
-            main, menu, nav, output, ruby, section, summary,
-            time, mark, audio, video {
-              margin: 0;
-              padding: 0;
-              border: 0;
-              font-size: 100%;
-              font: inherit;
-              vertical-align: baseline;
-            }
-            /* HTML5 display-role reset for older browsers */
-            article, aside, details, figcaption, figure,
-            footer, header, hgroup, main, menu, nav, section {
-              display: block;
-            }
-            /* HTML5 hidden-attribute fix for newer browsers */
-            *[hidden] {
-                display: none;
-            }
-            body {
-              line-height: 1;
-            }
-            ol, ul {
-              list-style: none;
-            }
-            blockquote, q {
-              quotes: none;
-            }
-            blockquote:before, blockquote:after,
-            q:before, q:after {
-              content: '';
-              content: none;
-            }
-            table {
-              border-collapse: collapse;
-              border-spacing: 0;
-            }
-          `}</style>
+          {/* Styles */}
+          <StyleReset />
+          <GlobalStyles />
 
-          <style jsx global>{`
-            body {
-              font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-            }
-          `}
-          </style>
+          {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js" /> */}
         </Head>
+
         <body>
           <Main />
           <NextScript />
