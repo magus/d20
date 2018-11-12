aa14147133055a23adbbcb507c70da258d0ff573
Noah Jorgensen <noah@iamnoah.com>
2018-11-12 00:37:22 -0800

    User type and build from id

--- app/types.js
+++ app/types.js
@@ -4,4 +4,7 @@ type Roll = DieRoll[];
 
 // Socket events
 export type RollEvent = { user: string, roll: Roll, time: number };
-export type ActiveUsers = { [userId: string]: any };
+export type User = { id: string, name: string, image: string | null };
+export type ActiveUsers = { [userId: string]: User | null };
+
+export const userFromId = (userId: string): User => ({ id: userId, name: userId.slice(0,4), image: null });
