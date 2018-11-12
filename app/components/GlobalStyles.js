a311f53238da035bcffc662115fbc33a2c59d1ae
Noah Jorgensen <noah@iamnoah.com>
2018-11-11 21:15:36 -0800

    global styles

---
+++ app/components/GlobalStyles.js
@@ -0,0 +1,11 @@
+// @flow
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  body {
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+      "Helvetica Neue", sans-serif;
+  }
+`;
