13a953366d7e5e54f1a3448de13b3ce4ef64f5de
Noah Jorgensen <noah@iamnoah.com>
2018-11-12 23:19:32 -0800

    username styling

--- app/components/Users.js
+++ app/components/Users.js
@@ -1,30 +1,49 @@
 // @flow
 import type { ActiveUsers } from "~/app/types";
-import { userFromId } from '~/app/types';
+import { userFromId } from "~/app/types";
 
 import React from "react";
 import styled from "styled-components";
 
 function User({ name }: { name: string }) {
-  return <div>{name}</div>;
+  return <Username>{name}</Username>;
 }
 
-export default ({ title = "Users", users }: { title?: string, users: ActiveUsers }) => {
+export default ({
+  title = "Users",
+  users
+}: {
+  title?: string,
+  users: ActiveUsers
+}) => {
   const userIds = Object.keys(users);
 
   return (
     <Container>
-      <h2>
-        {userIds.length} {title}
-      </h2>
+      <h2>{title}</h2>
 
-      {userIds.map(userId => {
-        const identity = users[userId] || userFromId(userId);
+      <Usernames>
+        {userIds.map(userId => {
+          const identity = users[userId] || userFromId(userId);
 
-        return <User key={userId} name={identity.name} />;
-      })}
+          return <User key={userId} name={identity.name} />;
+        })}
+      </Usernames>
     </Container>
   );
 };
 
 const Container = styled.div``;
+
+const Usernames = styled.div`
+  margin: 0 0 8px 0;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Username = styled.div`
+  border: 1px solid #aaa;
+  border-radius: 4px;
+  padding: 4px;
+  margin: 0 4px 0 0;
+`;
