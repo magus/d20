6bd75bb1989399f06876127098ac7b38df144e6f
magus <noah@iamnoah.com>
2018-12-28 02:01:41 -0800

    Page: prevent pull to refresh

--- app/components/GlobalStyles.js
+++ app/components/GlobalStyles.js
@@ -1,5 +1,5 @@
 // @flow
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   body {
@@ -7,5 +7,8 @@ export default createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
       "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
       "Helvetica Neue", sans-serif;
+
+    /* Disables pull-to-refresh but allows overscroll glow effects. */
+    overscroll-behavior-y: contain;
   }
 `;
