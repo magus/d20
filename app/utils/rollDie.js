e02bed0f29f938384ca1d045c9feab75401f80ad
magus <noah@iamnoah.com>
2019-01-01 22:27:16 -0800

    remove rollDie

--- app/utils/rollDie.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import type { DieRollType } from "~/app/types";
-
-export default function rollDie(d: number = 20, mod?: number = 0): DieRollType {
-  const dieRoll = Math.floor(Math.random() * d) + 1;
-  const result = dieRoll + mod;
-  return { d, result, mod };
-}
